aab61014001211286feca17692c0010f5e571695
Michael Ruvinshteyn <mruvinshteyn@stuy.edu>
2018-04-30 19:52:36 -0400

    added word count and most common word -- still missing group count

--- manifesto.py
+++ manifesto.py
@@ -1,16 +1,34 @@
+#The REDucers [Jawadul Kadir and Michael Ruvinshteyn]
+#SoftDev2 pd07
+#K18 -- Reductio ad Absurdum
+#2018 - 04 - 30
+
 from functools import reduce
-#import re
 
-manny = open("pg61.txt","rU")
-manny = manny.read()
-manny = manny.split("\n")
-manny = " ".join(manny)
+#set up string of all words - strip all punctuation and split by spaces
+manny = open("pg61.txt","rU").read()
+manny = " ".join(manny.split("\n"))
+manny = "".join(manny.split(";"))
+manny = "".join(manny.split(","))
+manny = "".join(manny.split(":"))
+manny = "".join(manny.split("."))
 manny = manny.split(" ")
-#manny = re.split(" ",manny)
-#print manny
-print reduce((lambda x, y: x + y), [1 if x.lower() == "bourgeoisie" else 0 for x in manny])
+manny = [x for x in manny if len(x) > 0]
 
-print reduce((lambda x, y: max(x,y)), manny)
+#count the amount of occurrences of the given word
+def wordCount(word):
+    return reduce((lambda x, y: x + y), [1 if x.lower() == word.lower() else 0 for x in manny])
 
+#return the most common word in the document
+#   groups all words in the set of all words (no dupes) with their frequencies
+#   reduces the list by continuing with the word that has the larger frequency
+def mostCommonWord():
+    def matchFreq(word):
+        return [word, wordCount(word)]
+    freqs = map(matchFreq, set(manny))
+    return reduce((lambda x,y:x if x[1] > y[1] else y), freqs)[0].lower()
 
-#print manny
+print wordCount("bourgeoisie")
+print wordCount("a")
+print wordCount("the")
+print mostCommonWord()
